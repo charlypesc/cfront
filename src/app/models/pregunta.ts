906bfdcf1faf0ed7cfad74924b202b3f838ea1de
Carlos Paredes <cparedes.escobar@gmail.com>
2021-06-16 20:08:19 -0400

    creacion de los modelos para las preguntas

---
+++ src/app/models/pregunta.ts
@@ -0,0 +1,12 @@
+import { Respuesta } from './respuesta';
+
+export class Pregunta{
+    descripcion: string;
+    listRespuesta: Respuesta[];
+    hide?: boolean;
+
+    constructor(descripcion: string, respuestas: Respuesta[]){
+        this.descripcion=descripcion;
+        this.listRespuesta=respuestas;
+    }
+}
